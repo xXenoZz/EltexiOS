f07fc392d16420f8729b4b4ba2e17c00c5fb01b2
xXenoZz <kostiakozlov2005@gmail.com>
2024-09-27 21:26:28 +0700

    Add files via upload

---
+++ first-lesson/first-lessonTests/first_lessonTests.swift
@@ -0,0 +1,17 @@
+//
+//  first_lessonTests.swift
+//  first-lessonTests
+//
+//  Created by Мак on 26.09.2024.
+//
+
+import Testing
+@testable import first_lesson
+
+struct first_lessonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
